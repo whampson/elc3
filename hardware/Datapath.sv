07cee52079a39012c985e59cb0919e9300893b59
Wes Hampson <whampso2@illinois.edu>
2017-04-11 12:27:26 -0500

    Add documentation

--- hardware/Datapath.sv
+++ hardware/Datapath.sv
@@ -6,6 +6,8 @@
 module Datapath
 (
     input   logic           Clk, Reset,
+    
+    /* eLC-3 datapath control signals */
     input   logic           LD_MAR, LD_MDR, LD_IR, LD_BEN, LD_REG, LD_CC, LD_PC, // Register load signals
     input   logic           GatePC, GateMDR, GateALU, GateMARMUX,                // Bus gates
     input   logic           ADDR1MUX,                                            // Mux select signals
@@ -14,15 +16,18 @@ module Datapath
     input   logic           MIO_EN, R_W                                          // RAM operation signals
 );
 
-    logic   [15:0]  MAR, MDR, IR, PC;
-    logic   [15:0]  MAR_In, MDR_In, IR_In, PC_In;
-    logic   [2:0]   SR1MUX_Out, SR2MUX_Out, DRMUX_Out;
-    logic   [15:0]  MARMUX_Out, MDRMUX_Out, PCMUX_Out, ADDR1MUX_Out, ADDR2MUX_Out;
-    logic   [15:0]  ALU;
-    logic   [15:0]  SR1, SR2;
-    logic   [15:0]  Bus;
-    logic           N, Z, P;
+    /* Internal signals */
+    logic   [15:0]  Bus;                                // The main data bus between CPU components
+    logic   [15:0]  MAR, MDR, IR, PC;                   // The current contents of MAR, MDR, IR, and PC
+    logic   [15:0]  MAR_In, MDR_In, IR_In, PC_In;       // Input signals for MAR, MDR, IR, and PC
+    logic   [2:0]   SR1MUX_Out, SR2MUX_Out, DRMUX_Out;  // Outputs of general purpose register selection MUXes
+    logic   [15:0]  MARMUX_Out, MDRMUX_Out, PCMUX_Out   // Outputs of MAR, MDR, and PC register data selection MUXes
+    logic   [15:0]  ADDR1MUX_Out, ADDR2MUX_Out;         // Outputs of memory addressing MUXes
+    logic   [15:0]  ALU;                                // Output of the ALU
+    logic   [15:0]  SR1, SR2;                           // Contents of SR1 and SR2 from register file
+    logic           N, Z, P;                            // Current contents of condition code registers (CCs)
     
+    /* Memory address register */
     Register        _MAR
     (
         .Clk(Clk),
@@ -32,6 +37,7 @@ module Datapath
         .Load(LD_MAR)
     );
     
+    /* Memory data register */
     Register        _MDR
     (
         .Clk(Clk),
@@ -41,6 +47,7 @@ module Datapath
         .Load(LD_MDR)
     );
     
+    /* Instruction register */
     Register        _IR
     (
         .Clk(Clk),
@@ -50,6 +57,7 @@ module Datapath
         .Load(LD_IR)
     );
     
+    /* Program counter (instruction pointer) */
     Register        _PC
     (
         .Clk(Clk),
@@ -59,6 +67,8 @@ module Datapath
         .Load(LD_PC)
     );
     
+    /* Module containing 8 general purpose registers (R0-R7)
+       and their selection logic */
     RegisterFile    _GenPurposeRegs
     (
         .Clk(Clk),
@@ -72,6 +82,8 @@ module Datapath
         .SR2_Out(SR2)
     );
     
+    /* MUX for choosing where to read the destination register
+       selection bits from */
     Mux_4to1 #(3)   _DRMUX
     (
         .In0(IR[11:9]),
@@ -82,6 +94,8 @@ module Datapath
         .Select(DRMUX)
     );
     
+    /* MUX for choosing where to read the source register 1
+       selection bits from */
     Mux_4to1 #(3)   _SR1MUX
     (
         .In0(IR[11:9]),
