2de96af08604946f254540b26d77cf37c1cdcf26
Wes Hampson <whampso2@illinois.edu>
2017-04-13 00:21:33 -0500

    Rename module instances

--- hardware/RegisterFile.sv
+++ hardware/RegisterFile.sv
@@ -1,10 +1,16 @@
+/**
+ * eLC-3 register file. Contains all general purpose registers (R0-R7) and
+ * register selection logic.
+ *
+ * @author Wes Hampson, Xavier Rocha
+ */
 module RegisterFile
 (
     input   logic           Clk, Reset,
-    input   logic           LD_REG,
-    input   logic   [2:0]   DR, SR1, SR2,
-    input   logic   [15:0]  In,
-    output  logic   [15:0]  SR1_Out, SR2_Out
+    input   logic           LD_REG,             // Register load signal
+    input   logic   [2:0]   DR, SR1, SR2,       // Register selection bits (000 is R0, 001 is R1, etc.)
+    input   logic   [15:0]  In,                 // Input data
+    output  logic   [15:0]  SR1_Out, SR2_Out    // Output of SR1 and SR2
 );
 
     // Internal load, input, and output signals
@@ -13,17 +19,17 @@ module RegisterFile
     logic   [15:0]  R0_Out, R1_Out, R2_Out, R3_Out, R4_Out, R5_Out, R6_Out, R7_Out;
     
     // General purpose registers
-    Register        R0(.Clk(Clk), .Reset(Reset), .In(R0_In), .Out(R0_Out), .Load(R0_Ld));
-    Register        R1(.Clk(Clk), .Reset(Reset), .In(R1_In), .Out(R1_Out), .Load(R1_Ld));
-    Register        R2(.Clk(Clk), .Reset(Reset), .In(R2_In), .Out(R2_Out), .Load(R2_Ld));
-    Register        R3(.Clk(Clk), .Reset(Reset), .In(R3_In), .Out(R3_Out), .Load(R3_Ld));
-    Register        R4(.Clk(Clk), .Reset(Reset), .In(R4_In), .Out(R4_Out), .Load(R4_Ld));
-    Register        R5(.Clk(Clk), .Reset(Reset), .In(R5_In), .Out(R5_Out), .Load(R5_Ld));
-    Register        R6(.Clk(Clk), .Reset(Reset), .In(R6_In), .Out(R6_Out), .Load(R6_Ld));
-    Register        R7(.Clk(Clk), .Reset(Reset), .In(R7_In), .Out(R7_Out), .Load(R7_Ld));
+    Register        _R0(.Clk(Clk), .Reset(Reset), .In(R0_In), .Out(R0_Out), .Load(R0_Ld));
+    Register        _R1(.Clk(Clk), .Reset(Reset), .In(R1_In), .Out(R1_Out), .Load(R1_Ld));
+    Register        _R2(.Clk(Clk), .Reset(Reset), .In(R2_In), .Out(R2_Out), .Load(R2_Ld));
+    Register        _R3(.Clk(Clk), .Reset(Reset), .In(R3_In), .Out(R3_Out), .Load(R3_Ld));
+    Register        _R4(.Clk(Clk), .Reset(Reset), .In(R4_In), .Out(R4_Out), .Load(R4_Ld));
+    Register        _R5(.Clk(Clk), .Reset(Reset), .In(R5_In), .Out(R5_Out), .Load(R5_Ld));
+    Register        _R6(.Clk(Clk), .Reset(Reset), .In(R6_In), .Out(R6_Out), .Load(R6_Ld));
+    Register        _R7(.Clk(Clk), .Reset(Reset), .In(R7_In), .Out(R7_Out), .Load(R7_Ld));
     
     // De-multiplexer for selecting destination register data line
-    DeMux_1to8      DRDecoder
+    DeMux_1to8      _DRSelect
     (
         .In(In),
         .Out0(R0_In),
@@ -38,7 +44,7 @@ module RegisterFile
     );
     
     // De-multiplexer for selecting destination register load line
-    DeMux_1to8 #(1) LDDecoder
+    DeMux_1to8 #(1) _LDSelect
     (
         .In(LD_REG),
         .Out0(R0_Ld),
@@ -53,7 +59,7 @@ module RegisterFile
     );
     
     // Multiplexer for selecting SR1 output
-    Mux_8to1        SR1_OutMux
+    Mux_8to1        _SR1_OutMux
     (
         .In0(R0_Out),
         .In1(R1_Out),
@@ -68,7 +74,7 @@ module RegisterFile
     );
     
     // Multiplexer for selecting SR2 output
-    Mux_8to1        SR2_OutMux
+    Mux_8to1        _SR2_OutMux
     (
         .In0(R0_Out),
         .In1(R1_Out),
