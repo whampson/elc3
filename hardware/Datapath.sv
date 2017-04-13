0b06913f5a96ab8b0dfd9a2f134d64e1bb180d61
Wes Hampson <whampso2@illinois.edu>
2017-04-13 00:21:59 -0500

    Documentation; rename 'IR_15_11' to 'IR_15_12'

--- hardware/Datapath.sv
+++ hardware/Datapath.sv
@@ -6,27 +6,23 @@
 module Datapath
 (
     input   logic           Clk, Reset,
-    
-    /* eLC-3 datapath control signals */
     input   logic   [15:0]  In,                                                  // Data from RAM
     input   logic           LD_MAR, LD_MDR, LD_IR, LD_BEN, LD_REG, LD_CC, LD_PC, // Register load signals
     input   logic           GatePC, GateMDR, GateALU, GateMARMUX,                // Bus gates
     input   logic           ADDR1MUX,                                            // Mux select signals
     input   logic   [1:0]   ADDR2MUX, PCMUX, DRMUX, SR1MUX, SR2MUX, MARMUX,      // Mux select signals
     input   logic   [1:0]   ALUK,                                                // ALU function select signal
-    input   logic           MIO_EN, /*R_W,*/                                         // RAM operation signals
+    input   logic           MIO_EN,                                              // RAM operation signals
     output  logic   [15:0]  Out,                                                 // Data to RAM
-    output  logic           BEN,
-    output  logic           IR_5,
-    output  logic   [3:0]   IR_15_11
-    //output  logic           R
+    output  logic           BEN,                                                 // Branch enable signal
+    output  logic           IR_5,                                                // Bit 5 of instruction register
+    output  logic   [3:0]   IR_15_12                                             // Bits 15-12 of instruction register
 );
 
-    /* Internal signals */
+    /* ==== Internal signals ==== */
     logic   [15:0]  Bus;                                // The main data bus between CPU components
     logic   [3:0]   Gate;                               // Concatenation of Gate* signals
     logic   [15:0]  MAR, MDR, IR, PC;                   // The current contents of MAR, MDR, IR, and PC
-    //logic   [15:0]  MAR_In, MDR_In, IR_In, PC_In;       // Input signals for MAR, MDR, IR, and PC
     logic   [2:0]   SR1MUX_Out, SR2MUX_Out, DRMUX_Out;  // Outputs of general purpose register selection MUXes
     logic   [15:0]  MARMUX_Out, MDRMUX_Out, PCMUX_Out;  // Outputs of MAR, MDR, and PC register data selection MUXes
     logic   [15:0]  ADDR1MUX_Out, ADDR2MUX_Out;         // Outputs of memory addressing MUXes
@@ -35,27 +31,24 @@ module Datapath
     logic   [15:0]  SR1, SR2;                           // Contents of SR1 and SR2 from register file
     logic           BEN_In;                             // Next state of branch enable register
     logic           N, Z, P;                            // Current contents of condition code registers (CCs)
-    logic           N_In, Z_In, P_In;                   // Condition code registers next states
-    
-    assign Out = MDR;
-    
-    /* ADDR is the sum of base address (ADDR1) and offset (ADDR2) */
-    assign ADDR = ADDR1MUX_Out + ADDR2MUX_Out;
-    
-    assign Gate = { GatePC, GateMDR, GateALU, GateMARMUX };
+    logic           N_In, Z_In, P_In;                   // Next states for condition code registers
     
     assign IR_5     = IR[5];
-    assign IR_15_11 = IR[15:11];
+    assign IR_15_12 = IR[15:12];
+    assign Out = MDR;                                       // Anything leaving the datapath comes from MDR
+    assign ADDR = ADDR1MUX_Out + ADDR2MUX_Out;              // Absolute memory address (Base + offset)
+    assign Gate = { GatePC, GateMDR, GateALU, GateMARMUX }; // Used below for Bus tri-state buffer selection
     
-    /* NZP logic */
+    /* ==== NZP logic ==== */
     assign N_In = Bus[15];
     assign Z_In = (Bus == 16'h0000) ? 1'b1 : 1'b0;
     assign P_In = ~N_In & ~Z_In;
     
-    /* BEN logic */
+    /* ==== Branch enable logic ==== */
     assign BEN_In = (IR[11] & N) | (IR[10] & Z) | (IR[9] & P);
     
-    /* Memory address register */
+    /* ==== Register definitions ==== */
+    // Memory address register
     Register        _MAR
     (
         .Clk(Clk),
@@ -65,7 +58,7 @@ module Datapath
         .Load(LD_MAR)
     );
     
-    /* Memory data register */
+    // Memory data register
     Register        _MDR
     (
         .Clk(Clk),
@@ -75,7 +68,7 @@ module Datapath
         .Load(LD_MDR)
     );
     
-    /* Instruction register */
+    // Instruction register
     Register        _IR
     (
         .Clk(Clk),
@@ -85,7 +78,7 @@ module Datapath
         .Load(LD_IR)
     );
     
-    /* Program counter (instruction pointer) */
+    // Program counter (instruction pointer)
     Register        _PC
     (
         .Clk(Clk),
@@ -95,7 +88,7 @@ module Datapath
         .Load(LD_PC)
     );
     
-    /* Branch enable status register */
+    // Branch enable status register
     Register #(1)   _BEN
     (
         .Clk(Clk),
@@ -105,7 +98,7 @@ module Datapath
         .Load(LD_BEN)
     );
     
-    /* Negative result status register */
+    // Negative result status register
     Register #(1)   _N
     (
         .Clk(Clk),
@@ -115,7 +108,7 @@ module Datapath
         .Load(LD_CC)
     );
     
-    /* Zero result status register */
+    // Zero result status register
     Register #(1)   _Z
     (
         .Clk(Clk),
@@ -125,7 +118,7 @@ module Datapath
         .Load(LD_CC)
     );
     
-    /* Positive result status register */
+    // Positive result status register
     Register #(1)   _P
     (
         .Clk(Clk),
@@ -135,8 +128,7 @@ module Datapath
         .Load(LD_CC)
     );
     
-    /* Module containing 8 general purpose registers (R0-R7)
-       and their selection logic */
+    // General purpose registers (R0-R7)
     RegisterFile    _GenPurposeRegs
     (
         .Clk(Clk),
@@ -150,8 +142,16 @@ module Datapath
         .SR2_Out(SR2)
     );
     
-    /* MUX for choosing where to read the destination register
-       selection bits from */
+    /* ==== The arithmetic and logic unit ==== */
+    ALU             _ALU
+    (
+        .A(SR1),
+        .B(SR2MUX_Out),
+        .Fn(ALUK)
+    );
+
+    /* ==== Multiplexer definitions ==== */
+    // MUX for choosing where destination register selection should be read from
     Mux_4to1 #(3)   _DRMUX
     (
         .In0(IR[11:9]),
@@ -162,8 +162,7 @@ module Datapath
         .Select(DRMUX)
     );
     
-    /* MUX for choosing where to read the source register 1
-       selection bits from */
+    // MUX for choosing where source register 1 selection bits should be read from
     Mux_4to1 #(3)   _SR1MUX
     (
         .In0(IR[11:9]),
@@ -174,7 +173,7 @@ module Datapath
         .Select(SR1MUX)
     );
     
-    /* MUX for selecting where to read input B of the ALU from */
+    // MUX for selecting where input B of the ALU should be read from
     Mux_2to1        _SR2MUX
     (
         .In0(SR2),
@@ -183,15 +182,7 @@ module Datapath
         .Select(SR2MUX)
     );
     
-    /* The ALU */
-    ALU             _ALU
-    (
-        .A(SR1),
-        .B(SR2MUX_Out),
-        .Fn(ALUK)
-    );
-    
-    /* MUX for selecting where the next value of PC is read from */
+    // MUX for selecting where the next value of PC is read from
     Mux_4to1        _PCMUX
     (
         .In0(PC + 16'd1),
@@ -202,7 +193,7 @@ module Datapath
         .Select(PCMUX)
     );
     
-    /* MUX for selecting where to read base address from */
+    // MUX for selecting where to read base memory address from
     Mux_2to1        _ADDR1MUX
     (
         .In0(PC),
@@ -211,7 +202,7 @@ module Datapath
         .Select(ADDR1MUX)
     );
     
-    /* MUX for selection where to read address offset from */
+    // MUX for selection where to read address offset from
     Mux_4to1        _ADDR2MUX
     (
         .In0(16'h0000),
@@ -222,7 +213,7 @@ module Datapath
         .Select(ADDR2MUX)
     );
     
-    /* MUX for selecting where to read the next value for MAR from */
+    // MUX for selecting where to read the next value for MAR from
     Mux_2to1        _MARMUX
     (
         .In0({ {8{1'b0}}, IR[7:0] }),       // ZEXT(IR[7:0])
@@ -231,7 +222,7 @@ module Datapath
         .Select(MARMUX)
     );
     
-    /* MUX for selecting where to read the next value for MDR from */
+    // MUX for selecting where to read the next value for MDR from
     Mux_2to1        _MDRMUX
     (
         .In0(Bus),
@@ -240,10 +231,11 @@ module Datapath
         .Select(MIO_EN)
     );
     
-    /* One-hot MUX for selecting which data line is currently allowed on the bus */
+    // One-hot MUX for selecting which data line is currently allowed on the bus
+    // (works like a tri-state buffer)
     MuxOneHot_4to1  _GateMux
     (
-        // Input order must follow definition of Gate (little-endian, see above)
+        // Input order must follow 'Gate' definition (little-endian, see above)
         .In0(MARMUX_Out),
         .In1(ALU),
         .In2(MDRMUX_Out),
