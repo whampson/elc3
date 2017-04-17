91fba579a20867fb008957f491d8eb27763ca314
Wes Hampson <whampso2@illinois.edu>
2017-04-16 22:01:29 -0500

    Add memory control unit and hex dusplays to toplevel

--- hardware/elc3.sv
+++ hardware/elc3.sv
@@ -10,11 +10,21 @@ module elc3
     input                   CLOCK_50,
     input           [3:0]   KEY,
     input           [17:0]  SW,
-    output  logic   [8:0]   LEDG,
-    output  logic   [17:0]  LEDR
+//    output  logic   [8:0]   LEDG,
+//    output  logic   [17:0]  LEDR,
+    output  logic   [6:0]   HEX0, HEX1, HEX2, HEX3,
+    output  logic           SRAM_CE_N, SRAM_OE_N, SRAM_WE_N,
+    output  logic           SRAM_LB_N, SRAM_UB_N,
+    output  logic   [19:0]  SRAM_ADDR,
+    inout   wire    [15:0]  SRAM_DQ
 );
 
-    // TODO: create the eLC-3!
+    // Synchronized master reset signal
+    logic           Reset;
+    
+    logic           Run;
+    
+    // eLC-3 control signals
     logic           LD_MAR, LD_MDR, LD_IR, LD_BEN, LD_REG, LD_CC, LD_PC; // Register load signals
     logic           GatePC, GateMDR, GateALU, GateMARMUX;                // Bus gates
     logic           ADDR1MUX;                                            // Mux select signals
@@ -25,8 +35,54 @@ module elc3
     logic           BEN;
     logic           IR_5;
     logic   [3:0]   IR_15_12;
-    logic   [15:0]  In, Out;
+    
+    // eLC-3 memory address signal
+    logic   [15:0]  Address;
+    
+    // eLC-3 data signals
+    logic   [15:0]  To_CPU, From_CPU;       // Data to/from datapath
+    logic   [15:0]  To_HexDisplays;         // "Video" output
+    logic   [15:0]  From_Switches;          // "Keyboard" input
 
-    Datapath dp(.Clk(CLOCK_50), .Reset(~KEY[0]), .*);
+    // eLC-3 datapath
+    Datapath dp
+    (
+        .Clk(CLOCK_50),
+        .In(To_CPU),
+        .Out(From_CPU),
+        .*
+    );
+    
+    // eLC-3 control unit
+    ControlUnit ctl
+    (
+        .Clk(CLOCK_50),
+        .*
+    );
+
+    // eLC-3 memory addressing unit, contains KBDR, KBSR, DDR, and DSR
+    MemoryControlUnit memsCtl
+    (
+        .Clk(CLOCK_50),
+        .Data_FromCPU(From_CPU),
+        .Data_ToCPU(To_CPU),
+        .Data_FromKeyboard(From_Switches),
+        .Data_ToVideo(To_HexDisplays),
+        .*
+    );
+
+    // 7-segment display converters
+    HexDriver hx0(.In(To_HexDisplays[3:0]),   .Out(HEX0));
+    HexDriver hx1(.In(To_HexDisplays[7:4]),   .Out(HEX1));
+    HexDriver hx2(.In(To_HexDisplays[11:8]),  .Out(HEX2));
+    HexDriver hx3(.In(To_HexDisplays[15:12]), .Out(HEX3));
+
+    // Switch synchronizer
+    Synchronizer #(16) syncSW(.Clk(CLOCK_50), .In(SW[15:0]), .Out(From_Switches));
+
+    // Reset signal synchronizer
+    Synchronizer syncReset(.Clk(CLOCK_50), .In(~KEY[0]), .Out(Reset));
+    
+    Synchronizer syncRun(.Clk(CLOCK_50), .In(~KEY[3]), .Out(Run));
 
 endmodule
