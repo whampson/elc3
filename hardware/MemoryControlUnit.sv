386070f316b52fab92196d3a0c7fcb1c018598c9
Wes Hampson <whampso2@illinois.edu>
2017-04-22 17:00:33 -0500

    Remove SRAM tristate buffer
    Moved to toplevel (elc3 module).

--- hardware/MemoryControlUnit.sv
+++ hardware/MemoryControlUnit.sv
@@ -11,60 +11,31 @@ module MemoryControlUnit
     input   logic           MIO_EN,
     input   logic           R_W,
     input   logic   [15:0]  Address,
+    input   logic   [15:0]  Data_FromSRAM,
     input   logic   [15:0]  Data_FromCPU,
     input   logic   [15:0]  Data_FromKeyboard,
+    output  logic           Mem_CE, Mem_OE, Mem_WE,
+    output  logic           Mem_LB, Mem_UB,
+    output  logic   [15:0]  Data_ToSRAM,
     output  logic   [15:0]  Data_ToCPU,
-    output  logic   [15:0]  Data_ToVideo,
-    output  logic           SRAM_CE_N, SRAM_OE_N, SRAM_WE_N,
-    output  logic           SRAM_LB_N, SRAM_UB_N,
-    output  logic   [19:0]  SRAM_ADDR,
-    inout   wire    [15:0]  SRAM_DQ
+    output  logic   [15:0]  Data_ToVideo
 );
 
     // Keyboard data and status; display data and status
     logic           LD_KBSR;
     logic           LD_DDR, LD_DSR;
-    logic   [15:0]  KBDR, KBDR_In, KBSR;
+    logic   [15:0]  KBDR, KBSR;
     logic   [15:0]  DDR, DSR;
     
     // Input MUX select signal
     logic   [1:0]   INMUX;
     
-    // Data to/from SRAM (passes through tri-state)
-    logic   [15:0]  Data_FromSRAM, Data_ToSRAM;
-    logic   [15:0]  TriState_In, TriState_Out;
-    
     assign Data_ToSRAM = Data_FromCPU;
+    assign Data_ToVideo = DDR;
     
-    assign Data_FromSRAM = TriState_Out;
-    assign TriState_In = Data_ToSRAM;
-    
-    always_ff @(posedge Clk) begin
-        Data_ToVideo <= DDR;
-        //KBDR_In <= Data_FromKeyboard;
-        //TriState_In <= Data_ToSRAM;
-        //Data_FromSRAM <= TriState_Out;
-        //SRAM_ADDR <= { 4'b0000, Address };
-    end
-
-    // Synchronized SRAM control signals (these are active-high)
-    logic           Mem_CE, Mem_CE_Out;
-    logic           Mem_OE, Mem_OE_Out;
-    logic           Mem_WE, Mem_WE_Out;
-    logic           Mem_LB, Mem_LB_Out;
-    logic           Mem_UB, Mem_UB_Out;
-    
-    assign SRAM_CE_N = ~Mem_CE_Out;
-    assign SRAM_OE_N = ~Mem_OE_Out;
-    assign SRAM_WE_N = ~Mem_WE_Out;
-    assign SRAM_LB_N = ~Mem_LB_Out;
-    assign SRAM_UB_N = ~Mem_UB_Out;
-    
-    // Memory addressing and control
-    assign          SRAM_ADDR = {4'b0000, Address };    // SRAM is 1M x 16, eLC-3 memory is only 64K x 16
-    assign          Mem_CE = 1'b1;
-    assign          Mem_LB = 1'b1;
-    assign          Mem_UB = 1'b1;
+    assign Mem_CE = 1'b1;
+    assign Mem_LB = 1'b1;
+    assign Mem_UB = 1'b1;
     
     // Memory-mapped I/O logic
     always_comb begin
@@ -77,7 +48,7 @@ module MemoryControlUnit
         
         unique case (Address)
             // Read/write KBSR
-            16'hFE00:   begin
+            16'hFE00: begin
                 if (MIO_EN) begin
                     if (R_W)
                         LD_KBSR = 1'b1;
@@ -87,13 +58,13 @@ module MemoryControlUnit
             end
             
             // Read KBDR
-            16'hFE02:   begin
+            16'hFE02: begin
                 if (MIO_EN)
                     INMUX = 2'b10;
             end
             
             // Read/write DSR
-            16'hFE04:   begin
+            16'hFE04: begin
                 if (MIO_EN) begin
                     if (R_W)
                         LD_DSR = 1'b1;
@@ -103,15 +74,14 @@ module MemoryControlUnit
             end
             
             // Write DDR
-            16'hFE06:   begin
-                if (MIO_EN) begin
-                    if (R_W)
-                        LD_DDR = 1'b1;
+            16'hFE06: begin
+                if (MIO_EN && R_W) begin
+                    LD_DDR = 1'b1;
                 end
             end
             
             // Read/write SRAM
-            default :   begin
+            default : begin
                 if (MIO_EN) begin
                     if (R_W)
                         Mem_WE = 1'b1;
@@ -170,21 +140,21 @@ module MemoryControlUnit
         .Select(INMUX)
     );
     
-    // Tri-state buffer for data lines between eLC-3 memory control logic and SRAM chip
-    BidirectionalTriState memTristate
-    (
-        .Clk(Clk),
-        .In(TriState_In),           // Data going into tri-state to memory
-        .Out(TriState_Out),         // Data coming out of tri-state from memory
-        .Data(SRAM_DQ),             // Bus travelling between tri-state and SRAM chip
-        .WriteEnable(Mem_WE_Out)    // Read or write to SRAM
-    );
-    
-    // SRAM control signal synchronizers
-    Synchronizer syncCE(.*, .In(Mem_CE), .Out(Mem_CE_Out));
-    Synchronizer syncOE(.*, .In(Mem_OE), .Out(Mem_OE_Out));
-    Synchronizer syncWE(.*, .In(Mem_WE), .Out(Mem_WE_Out));
-    Synchronizer syncLB(.*, .In(Mem_LB), .Out(Mem_LB_Out));
-    Synchronizer syncUB(.*, .In(Mem_UB), .Out(Mem_UB_Out));
+//    // Tri-state buffer for data lines between eLC-3 memory control logic and SRAM chip
+//    BidirectionalTriState memTristate
+//    (
+//        .Clk(Clk),
+//        .In(TriState_In),           // Data going into tri-state to memory
+//        .Out(TriState_Out),         // Data coming out of tri-state from memory
+//        .Data(SRAM_DQ),             // Bus travelling between tri-state and SRAM chip
+//        .WriteEnable(Mem_WE_Out)    // Read or write to SRAM
+//    );
+//    
+//    // SRAM control signal synchronizers
+//    Synchronizer syncCE(.*, .In(Mem_CE), .Out(Mem_CE_Out));
+//    Synchronizer syncOE(.*, .In(Mem_OE), .Out(Mem_OE_Out));
+//    Synchronizer syncWE(.*, .In(Mem_WE), .Out(Mem_WE_Out));
+//    Synchronizer syncLB(.*, .In(Mem_LB), .Out(Mem_LB_Out));
+//    Synchronizer syncUB(.*, .In(Mem_UB), .Out(Mem_UB_Out));
 
 endmodule
