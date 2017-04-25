aa46bc430fdbb40a858d14d11ee9a5c684251a89
Wes Hampson <whampso2@illinois.edu>
2017-04-24 21:29:18 -0500

    Add MUL test program

--- hardware/FakeMemory.sv
+++ hardware/FakeMemory.sv
@@ -38,17 +38,17 @@ module FakeMemory
         if (Reset) begin
             // Initial memory contents
             MemoryArray[0]  <= 16'b0000_0000_0000_0000;     // NOP
-            MemoryArray[1]  <= 16'b1010_111_000000001;      // LDI R7,#1
-            MemoryArray[2]  <= 16'b1100_000_111_000000;     // JMP R7
+            MemoryArray[1]  <= 16'b1010_111_000000001;      // LDI  R7,#1
+            MemoryArray[2]  <= 16'b1100_000_111_000000;     // JMP  R7
             MemoryArray[3]  <= 16'hFE02;
-            MemoryArray[4]  <= 16'h0000;
-            MemoryArray[5]  <= 16'b0001_011_001_1_01010;    // ADD R3,R1,#10
-            MemoryArray[6]  <= 16'b1011_011_111111101;      // STI R3,#-3
-            MemoryArray[7]  <= 16'b1100_000_111_000000;     // RET
-            MemoryArray[8]  <= 16'h0000;
-            MemoryArray[9]  <= 16'h0000;
-            MemoryArray[10] <= 16'h0000;
-            MemoryArray[11] <= 16'h0000;
+            MemoryArray[4]  <= 16'hFE06;
+            MemoryArray[5]  <= 16'b0001_011_001_1_01010;    // ADD  R3,R3,#10
+            MemoryArray[6]  <= 16'b0001_101_101_1_10000;    // ADD  R5,R5,#-16
+            MemoryArray[7]  <= 16'b1101_001_011_0_00_101;   // MUL  R1,R3,R5
+            MemoryArray[8]  <= 16'b1101_010_011_1_00100;    // MUL  R2,R3,#4
+            MemoryArray[9]  <= 16'b1011_011_111111011;      // STI  R1,#-6
+            MemoryArray[10] <= 16'b1011_011_111111011;      // STI  R2,#-7
+            MemoryArray[11] <= 16'b1111_0000_00100101;      // TRAP x25
             MemoryArray[12] <= 16'h0000;
             MemoryArray[13] <= 16'h0000;
             MemoryArray[14] <= 16'h0000;
@@ -74,7 +74,7 @@ module FakeMemory
             MemoryArray[34] <= 16'h0000;
             MemoryArray[35] <= 16'h0000;
             MemoryArray[36] <= 16'h0000;
-            MemoryArray[37] <= 16'h0030;                    // HALT trapvector
+            MemoryArray[37] <= 16'h0030;                    // TRAP x25 (HALT)
             MemoryArray[38] <= 16'h0000;
             MemoryArray[39] <= 16'h0000;
             MemoryArray[40] <= 16'h0000;
@@ -85,7 +85,7 @@ module FakeMemory
             MemoryArray[45] <= 16'h0000;
             MemoryArray[46] <= 16'h0000;
             MemoryArray[47] <= 16'h0000;
-            MemoryArray[48] <= 16'b1101_000000000000;       // HALT
+            MemoryArray[48] <= 16'b0000_111_111111111;       // TRAP_HALT   BRnzp #-1
             MemoryArray[49] <= 16'h0000;
             MemoryArray[50] <= 16'h0000;
             MemoryArray[51] <= 16'h0000;
